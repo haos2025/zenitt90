75590765f0f46576765173fa933c739bb72abae8
haos2025 <shadowcloud488@gmail.com>
2026-08-13 04:37:56 +0500

    build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -146,6 +146,8 @@ dependencies {
     implementation(libs.hilt.android)
     ksp(libs.hilt.compiler)
     implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.androidx.hilt.work)
+    ksp(libs.androidx.hilt.compiler)
     
     // QR / Camera
     implementation(libs.zxing.core)
